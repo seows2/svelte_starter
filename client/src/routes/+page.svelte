f978dc41b49fed52ac18154aeb8c2201cf80d36e
seows2 <seows2@naver.com>
2023-02-05 01:30:05 +0900

    feat: 초기 커밋

---
+++ client/src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<h1>하이</h1>
+</section>
+
+<style>
+
+</style>
---
+++ client/src/routes/about/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+
+</script>
+
+<section>
+	<h1>하이 about</h1>
+</section>
+
+<style>
+
+</style>
